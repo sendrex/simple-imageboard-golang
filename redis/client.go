b90de185187d34018eb31918343f9c8748c8894c
AquoDev <14019792+AquoDev@users.noreply.github.com>
2019-07-10 15:08:42 +0200

    Completed Redis client

--- redis/client.go
+++ redis/client.go
@@ -1,15 +1,41 @@
 package redis
 
-import "github.com/go-redis/redis"
+import (
+	"fmt"
+	"os"
+	"strconv"
+
+	redis "github.com/go-redis/redis"
+)
 
 // Redis client connection.
-var client = redis.NewClient(&redis.Options{
-	Addr:     "localhost:6379",
-	Password: "", // no password set
-	DB:       0,  // use default DB
-})
+var client *redis.Client
 
-// GetRedisClient returns an already existing Redis client for caching.
-func GetRedisClient() (*redis.Client) {
+// GetRedisClient returns an already connected Redis client for caching.
+func GetRedisClient() *redis.Client {
 	return client
+}
+
+func init() {
+	// Get Redis host, port, password and database
+	host := os.Getenv("REDIS_HOST")
+	password := os.Getenv("REDIS_PASSWORD")
+	port, err := strconv.Atoi(os.Getenv("REDIS_PORT"))
+	if err != nil {
+		panic(err)
+	}
+	db, err := strconv.Atoi(os.Getenv("REDIS_DATABASE"))
+	if err != nil {
+		panic(err)
+	}
+
+	// Parse host and port into accepted address
+	addr := fmt.Sprintf("%s:%d", host, port)
+
+	// Connect the client
+	client = redis.NewClient(&redis.Options{
+		Addr:     addr,
+		Password: password,
+		DB:       db,
+	})
 }
