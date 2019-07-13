b6867a8db98c65fb7226135b675e4b6ae81e2f74
AquoDev <14019792+AquoDev@users.noreply.github.com>
2019-07-13 10:56:41 +0200

    Added function to Redis utils

--- redis/utils.go
+++ redis/utils.go
@@ -7,7 +7,12 @@ func makeKey(prefix string, number uint64) string {
 	return fmt.Sprintf("%s:%d", prefix, number)
 }
 
-// GetPageKey returns a string that corresponds to that key in Redis.
+// GetPageKey returns a string that corresponds to a 'page' key in Redis.
 func GetPageKey(id uint64) string {
 	return makeKey("page", id)
+}
+
+// GetThreadKey returns a string that corresponds to a 'thread' key in Redis.
+func GetThreadKey(id uint64) string {
+	return makeKey("thread", id)
 }
