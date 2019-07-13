0d69e69ac2031d75c3a02a59f995e79c4beb0a9e
AquoDev <14019792+AquoDev@users.noreply.github.com>
2019-07-13 17:33:18 +0200

    Updated Redis utils

--- redis/utils.go
+++ redis/utils.go
@@ -16,3 +16,8 @@ func GetPageKey(id uint64) string {
 func GetThreadKey(id uint64) string {
 	return makeKey("thread", id)
 }
+
+// GetPostKey returns a string that corresponds to a 'post' key in Redis.
+func GetPostKey(id uint64) string {
+	return makeKey("post", id)
+}
