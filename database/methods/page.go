f544b537aedb43dc63c1c14fe2b96313b56117d8
AquoDev <14019792+AquoDev@users.noreply.github.com>
2019-07-13 17:28:35 +0200

    Predefined structs of Page and Thread

--- database/methods/page.go
+++ database/methods/page.go
@@ -6,12 +6,15 @@ import (
 	"github.com/AquoDev/simple-imageboard-golang/database"
 )
 
+// Page has a 'posts' slice to be filled when it's requested.
+type Page struct {
+	Posts []database.Post
+}
+
 // GetPage returns a JSON with a thread list.
 func GetPage(id uint64) (string, error) {
-	// Make empty struct of posts slice
-	var page struct {
-		Posts []database.Post
-	}
+	// Make empty page
+	page := new(Page)
 
 	// Query posts that started a thread (on_thread == null)
 	err := db.Select("id, content, pic, created_at, updated_at").Offset(10 * id).Limit(10).Where("on_thread IS NULL").Order("updated_at desc").Find(&page.Posts).Error
--- database/methods/thread.go
+++ database/methods/thread.go
@@ -6,12 +6,15 @@ import (
 	"github.com/AquoDev/simple-imageboard-golang/database"
 )
 
+// Thread has a 'posts' slice to be filled when it's requested.
+type Thread struct {
+	Posts []database.Post
+}
+
 // GetThread returns a JSON with a thread (original post + on_thread == original post ID).
 func GetThread(id uint64) (string, error) {
-	// Make empty struct of posts slice
-	var thread struct {
-		Posts []database.Post
-	}
+	// Make empty thread
+	thread := new(Thread)
 
 	// Query posts that belong to a thread
 	err := db.Select("id, content, pic, reply_to, created_at").Where("id = ?", id).Or("on_thread = ?", id).Order("id asc").Find(&thread.Posts).Error
