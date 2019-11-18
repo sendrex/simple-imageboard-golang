7571da7571b5dc943bec5690b82bd96ed332a9bf
AquoDev <14019792+AquoDev@users.noreply.github.com>
2019-11-18 14:01:44 +0100

    Now all nullable fields are hidden if they're null

--- database/model.go
+++ database/model.go
@@ -7,13 +7,13 @@ import (
 
 // Post defines the table in which the posts will be saved and how it's represented in JSON.
 type Post struct {
-	ID         uint64      `json:"id" gorm:"primary_key;AUTO_INCREMENT"`
-	Content    string      `json:"content" gorm:"not null;size:1000"`
-	OnThread   *null.Int   `json:"on_thread,omitempty"`
-	Pic        null.String `json:"pic,omitempty" gorm:"size:512"`
-	DeleteCode string      `json:"delete_code,omitempty" gorm:"not null"`
-	CreatedAt  *time.Time  `json:"created_at" gorm:"not null;default:CURRENT_TIMESTAMP"`
-	UpdatedAt  *time.Time  `json:"updated_at,omitempty" gorm:"not null;default:CURRENT_TIMESTAMP"`
+	ID         uint64       `json:"id" gorm:"primary_key;AUTO_INCREMENT"`
+	Content    string       `json:"content" gorm:"not null;size:1000"`
+	OnThread   *null.Int    `json:"on_thread,omitempty"`
+	Pic        *null.String `json:"pic,omitempty" gorm:"size:512"`
+	DeleteCode string       `json:"delete_code,omitempty" gorm:"not null;size:128"`
+	CreatedAt  *time.Time   `json:"created_at" gorm:"not null;default:CURRENT_TIMESTAMP"`
+	UpdatedAt  *time.Time   `json:"updated_at,omitempty" gorm:"not null;default:CURRENT_TIMESTAMP"`
 }
 
 func init() {
