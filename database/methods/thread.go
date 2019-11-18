02d32feebfa91b1fd78dac5e1b06b6bac933027c
AquoDev <14019792+AquoDev@users.noreply.github.com>
2019-11-18 12:44:22 +0100

    Added delete func for old threads

--- database/methods/thread.go
+++ database/methods/thread.go
@@ -11,6 +11,11 @@ type Thread struct {
 	Posts []database.Post
 }
 
+// ThreadsToDelete has a 'posts' slice where we'll save old threads to delete.
+type ThreadsToDelete struct {
+	Collection []database.Post
+}
+
 // GetThread returns a JSON with a thread (original post + on_thread == original post ID).
 func GetThread(id uint64) (string, error) {
 	// Make empty thread
@@ -26,4 +31,24 @@ func GetThread(id uint64) (string, error) {
 	result, err := json.Marshal(thread.Posts)
 
 	return string(result), err
+}
+
+// DeleteOldThreads deletes any thread older than the last bump date in page 9.
+func DeleteOldThreads() (err error) {
+	// Make empty collection
+	threads := new(ThreadsToDelete)
+
+	// Query old threads and save them in the collection
+	result := db.Where("on_thread IS NULL").Order("updated_at desc").Offset(300).Find(&threads.Collection)
+
+	// Check if any threads have been found
+	if result.RecordNotFound() {
+		// If any record hasn't been found, return the error
+		err = result.Error
+	} else {
+		// Delete old threads inside the collection
+		err = db.Delete(&threads.Collection).Error
+	}
+
+	return
 }
