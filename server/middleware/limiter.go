83444ddf0a8665eaafa78ae536b60904e1369cb9
AquoDev <14019792+AquoDev@users.noreply.github.com>
2019-11-19 23:03:37 +0100

    Fixed limiters

--- server/middleware/limiter.go
+++ server/middleware/limiter.go
@@ -24,22 +24,19 @@ func StrictLimiter() *limiter.Limiter {
 	return strict
 }
 
-func configureLimiter(requestPerSecond float64, windowDuration time.Duration) (lmt *limiter.Limiter) {
+func configureLimiter(requestsPerSecond float64) (lmt *limiter.Limiter) {
 	var message interface{}
 	message = handler.GetError(429)
 	message, _ = json.Marshal(message)
 	jsonMessage := string(message.([]byte))
 
-	lmt = tollbooth.NewLimiter(requestPerSecond, nil)
-	lmt.SetTokenBucketExpirationTTL(windowDuration * time.Second)
-	lmt.SetMessage(jsonMessage)
-	lmt.SetMessageContentType("application/json; charset=UTF-8")
+	lmt = tollbooth.NewLimiter(requestsPerSecond, &limiter.ExpirableOptions{DefaultExpirationTTL: time.Hour}).SetMessage(jsonMessage).SetMessageContentType("application/json; charset=UTF-8").SetStatusCode(200)
 
 	return
 }
 
 func init() {
-	regular = configureLimiter(3, 15)
-	strict = configureLimiter(0.1, 15)
+	regular = configureLimiter(3)
+	strict = configureLimiter(1)
 	fmt.Println("Limiters are now configured")
 }
