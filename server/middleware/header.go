1dcccb52ed3f92f61a83d5dc3646a424e82ca341
AquoDev <14019792+AquoDev@users.noreply.github.com>
2019-11-19 14:14:32 +0100

    Updated header for requests

--- server/middleware/header.go
+++ server/middleware/header.go
@@ -1,26 +1,16 @@
 package middleware
 
 import (
-	"strings"
-
 	"github.com/AquoDev/simple-imageboard-golang/server/handler"
 	"github.com/kataras/iris"
 )
 
-// UseContentTypeJSON sets the "Content-Type" header to "application/json; charset=utf-8".
-func UseContentTypeJSON(ctx iris.Context) {
-	ctx.Header("Content-Type", "application/json; charset=utf-8")
-	ctx.Next()
-}
-
 // CheckHeaders handles a response with an error if any check fails.
 func CheckHeaders(ctx iris.Context) {
-	contentType := ctx.GetHeader("Content-Type")
-
 	// Check "Content-Type"
-	if strings.ToLower(contentType) != "application/json; charset=utf-8" {
+	if ctx.GetHeader("Content-Type") != "application/json; charset=UTF-8" {
 		invalidHeader := handler.GetError(400)
-		ctx.WriteString(invalidHeader)
+		ctx.JSON(invalidHeader)
 		return
 	}
 
