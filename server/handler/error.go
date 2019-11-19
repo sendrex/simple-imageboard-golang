401a835fd056fd86fef3bc004fe2add93195d3a4
AquoDev <14019792+AquoDev@users.noreply.github.com>
2019-11-19 14:16:57 +0100

    Better error handling

--- server/handler/error.go
+++ server/handler/error.go
@@ -1,29 +1,22 @@
 package handler
 
-import (
-	"encoding/json"
-
-	"github.com/kataras/iris"
-)
-
-type errorJSON struct {
-	Status  uint16 `json:"status"`
-	Message string `json:"message"`
-}
+import "github.com/kataras/iris"
 
 var (
-	responses = make(map[uint16]string)
-	errors    = map[uint16]string{
+	errors = map[uint16]string{
 		400: "Bad Request",
 		404: "Not Found",
 		500: "Internal Server Error",
 	}
 )
 
-// GetError returns a JSON string with a <status> <message> response.
-func GetError(status uint16) (response string) {
-	if value, ok := responses[status]; ok {
-		response = value
+// GetError returns a map structure with a <status> <message> response.
+func GetError(status uint16) (response map[string]interface{}) {
+	if message, ok := errors[status]; ok {
+		response = map[string]interface{}{
+			"message": message,
+			"status":  status,
+		}
 	} else {
 		response = GetError(500)
 	}
@@ -33,19 +26,5 @@ func GetError(status uint16) (response string) {
 // PathNotFound handles a failed request with a 404 Not Found response.
 func PathNotFound(ctx iris.Context) {
 	response := GetError(404)
-	ctx.WriteString(response)
-}
-
-func makeResponseError(status uint16, message string) string {
-	jsonObject, _ := json.Marshal(&errorJSON{
-		Status:  status,
-		Message: message,
-	})
-	return string(jsonObject)
-}
-
-func init() {
-	for status, message := range errors {
-		responses[status] = makeResponseError(status, message)
-	}
+	ctx.JSON(response)
 }
