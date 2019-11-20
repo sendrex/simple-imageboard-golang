473dbe20e6166d10594dce11824b47c5c3ee5a89
AquoDev <14019792+AquoDev@users.noreply.github.com>
2019-11-20 12:39:32 +0100

    Moved makeKey redis function

--- redis/internal.go
+++ redis/internal.go
@@ -1,8 +1,15 @@
 package redis
 
 import (
+	"fmt"
+
 	"github.com/AquoDev/simple-imageboard-golang/model"
 )
+
+// makeKey returns a string built with both arguments.
+func makeKey(prefix string, number uint64) string {
+	return fmt.Sprintf("%s:%d", prefix, number)
+}
 
 // getCachedPage returns a cached post slice (used for pages and threads).
 func getCachedPostSlice(key string) ([]model.Post, error) {
--- redis/keys.go
+++ redis/keys.go
@@ -1,14 +1,5 @@
 package redis
 
-import (
-	"fmt"
-)
-
-// makeKey returns a string built with both arguments.
-func makeKey(prefix string, number uint64) string {
-	return fmt.Sprintf("%s:%d", prefix, number)
-}
-
 // GetPageKey returns a string that corresponds to a 'page' key in Redis.
 func GetPageKey(id uint64) string {
 	return makeKey("page", id)
