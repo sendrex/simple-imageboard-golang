81c9b558a09914c2e294b57351ec285612c73e71
AquoDev <14019792+AquoDev@users.noreply.github.com>
2020-07-22 11:18:46 +0200

    Added validation and handling functions for post model

--- model/post.go
+++ model/post.go
@@ -3,16 +3,19 @@ package model
 import (
 	"errors"
 	"math/rand"
+	"regexp"
 	"time"
 
 	"github.com/AquoDev/simple-imageboard-golang/env"
-	"github.com/asaskevich/govalidator"
 	"github.com/jinzhu/gorm"
 )
 
-var tableName = env.GetString("DB_TABLE_NAME")
+var (
+	tableName = env.GetString("DB_TABLE_NAME")
+	regex     = regexp.MustCompile(`^(http(s?):)([/|.|\w|\s|-])*\.(?:jp(e?)g|gif|png|webm|webp)$`)
+)
 
-// Post defines the table in which the posts will be saved and how it's represented in JSON.
+// Post is the main struct. It defines how posts will be stored and represented.
 type Post struct {
 	ID           uint64     `json:"id" gorm:"primary_key;AUTO_INCREMENT"`
 	Content      string     `json:"content" gorm:"not null;size:1000"`
@@ -22,6 +25,7 @@ type Post struct {
 	ReplyTo      *uint64    `json:"reply_to,omitempty"`
 	CreatedAt    *time.Time `json:"created_at" gorm:"not null;default:CURRENT_TIMESTAMP"`
 	UpdatedAt    *time.Time `json:"updated_at,omitempty" gorm:"not null;default:CURRENT_TIMESTAMP"`
+	Sage         bool       `json:"sage,omitempty" gorm:"-"` // This field is not stored
 }
 
 // TableName sets the table name for the model Post.
@@ -29,44 +33,66 @@ func (post *Post) TableName() string {
 	return tableName
 }
 
-// BeforeCreate returns an error if this post isn't valid, and nil otherwise.
+// BeforeCreate makes the delete code if it's empty.
 func (post *Post) BeforeCreate(scope *gorm.Scope) error {
-	// Check if content has at least one character
-	if len(post.Content) == 0 {
-		return errors.New("content must have at least 1 character")
-	}
-
-	// Check if content has surpassed 1000 characters
-	if len(post.Content) > 1000 {
-		return errors.New("content must not have more than 1000 characters")
-	}
-
-	// Check if delete_code has surpassed 128 characters
-	if len(post.DeleteCode) > 128 {
-		return errors.New("delete_code must not have more than 128 characters")
-	}
-
-	// Check if it has already an ID (it shouldn't have)
-	if post.ID != 0 {
-		return errors.New("id shouldn't be set")
-	}
-
-	// Check if pic exists and it's an invalid URL (it should be a valid URL)
-	if post.Pic != nil && len(*post.Pic) > 512 {
-		return errors.New("pic must not have more than 512 characters")
-	}
-
-	// Check if pic exists and it's an invalid URL (it should be a valid URL)
-	if post.Pic != nil && !govalidator.IsURL(*post.Pic) {
-		return errors.New("pic is a invalid url")
-	}
-
 	// Make delete code if it hasn't one
 	if post.DeleteCode == "" {
 		post.DeleteCode = randomString(32)
 	}
 
 	return nil
+}
+
+// Validate checks if all fields are valid.
+func (post *Post) Validate() error {
+	if post.ID != 0 {
+		// Check if post hasn't got an ID
+		return errors.New("id must not be set")
+	} else if len(post.Content) == 0 {
+		// Check if content has at least one character
+		return errors.New("content must not be empty")
+	} else if len(post.Content) > 1000 {
+		// Check if content hasn't more than 1000 characters
+		return errors.New("content must not have more than 1000 characters")
+	} else if len(post.DeleteCode) > 128 {
+		// Check if delete_code hasn't more than 128 characters
+		return errors.New("delete_code must not have more than 128 characters")
+	} else if post.Pic != nil {
+		// Check if pic is not empty and...
+		if len(*post.Pic) > 512 {
+			// ... it hasn't more than 512 characters
+			return errors.New("pic must not have more than 512 characters")
+		} else if !regex.MatchString(*post.Pic) {
+			// ... it's a valid URL
+			return errors.New("pic must be a valid URL")
+		}
+	}
+
+	return nil
+}
+
+// RepliesToAnotherPost checks if this post replies to another post.
+func (post *Post) RepliesToAnotherPost() bool {
+	return post.ReplyTo != nil
+}
+
+// IsAParentThread checks if this post starts a parent thread.
+func (post *Post) IsAParentThread() bool {
+	return post.ParentThread == nil
+}
+
+// Sages checks if parent thread must not be bumped.
+func (post *Post) Sages() bool {
+	return post.Sage
+}
+
+// MakeRelationWith adds the parent thread ID to the post based on the reply.
+func (post *Post) MakeRelationWith(reply *Post) {
+	if reply.IsAParentThread() {
+		post.ParentThread = &reply.ID
+	} else {
+		post.ParentThread = reply.ParentThread
+	}
 }
 
 // randomString returns a random string from predefined characters.
