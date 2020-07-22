e16b819dd20541be296a91f32813ba860a3c293b
AquoDev <14019792+AquoDev@users.noreply.github.com>
2020-07-22 11:17:57 +0200

    Added validation function for delete_data model

--- model/delete_data.go
+++ model/delete_data.go
@@ -1,7 +1,20 @@
 package model
 
-// DeleteData is a struct in which data to delete a post is parsed from/to JSON.
+import "errors"
+
+// DeleteData is a struct used for incoming DELETE requests or successful POST requests.
 type DeleteData struct {
 	ID         uint64 `json:"id"`
 	DeleteCode string `json:"delete_code"`
+}
+
+// Validate checks if all fields are valid.
+func (data *DeleteData) Validate() error {
+	if data.ID == 0 {
+		return errors.New("id must be set")
+	} else if data.DeleteCode == "" {
+		return errors.New("delete_code must not be empty")
+	}
+
+	return nil
 }
