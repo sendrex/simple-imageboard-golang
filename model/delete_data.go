042cc0e10861fa0a78f7c34c0aa38b54f30a591e
AquoDev <14019792+AquoDev@users.noreply.github.com>
2020-08-06 21:07:07 +0200

    Renamed delete_code to password

--- model/delete_data.go
+++ model/delete_data.go
@@ -4,16 +4,16 @@ import "errors"
 
 // DeleteData is a struct used for incoming DELETE requests or successful POST requests.
 type DeleteData struct {
-	ID         uint64 `json:"id"`
-	DeleteCode string `json:"delete_code"`
+	ID       uint64 `json:"id"`
+	Password string `json:"password"`
 }
 
 // Validate checks if all fields are valid.
 func (data *DeleteData) Validate() error {
 	if data.ID == 0 {
 		return errors.New("id must be set")
-	} else if data.DeleteCode == "" {
-		return errors.New("delete_code must not be empty")
+	} else if data.Password == "" {
+		return errors.New("password must not be empty")
 	}
 
 	return nil
